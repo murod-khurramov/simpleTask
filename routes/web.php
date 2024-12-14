6fe105d6525312abff56655876a88807c96ca2ea
Khurramov <murod_khurramov@icloud.com>
2024-12-14 15:59:22 +0500

    Update web routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use \App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/profile', [PageController::class, 'profile'])->name('profile');
+Route::get('/dashboard', [PageController::class, 'dashboard'])->name('dashboard');
+Route::get('/calendar', [PageController::class, 'calendar'])->name('calendar');
+Route::get('/days', [PageController::class, 'days'])->name('days');
+Route::get('/project', [PageController::class, 'project'])->name('project');
 
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
